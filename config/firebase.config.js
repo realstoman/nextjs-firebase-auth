27f0d6e84b2e37a7e144250686cd7fc267ac8aba
realstoman <realstoman@gmail.com>
2023-02-05 21:09:40 +0430

    Add firebase settings

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,17 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getApps, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyDSIxmaKLHKxCm-m-ZuVCcL9sdyMP9CQnA',
-	authDomain: 'nextjs-firebase-auth-e43f8.firebaseapp.com',
-	projectId: 'nextjs-firebase-auth-e43f8',
-	storageBucket: 'nextjs-firebase-auth-e43f8.appspot.com',
-	messagingSenderId: '1033865694385',
-	appId: '1:1033865694385:web:144bdd42bca497de5ba690',
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	// measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+if (!getApps().length) {
+	initializeApp(firebaseConfig);
+}
+// Initialize Firebase auth
+export const auth = getAuth();
