88d1f13e167b88c18d6a3150b9a60f9672422151
realstoman <realstoman@gmail.com>
2023-02-06 13:43:01 +0430

    Update footer link

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,16 +1,13 @@
 function Footer() {
 	return (
 		<>
-			<div className="flex items-center justify-center mt-14 bg-gray-700 rounded-md px-4 shadow-sm">
-				<p className="text-2xl md-text-3xl text-primary-dark dark:text-primary-light mb-5 mr-2 mt-4 text-gray-200 ">
-					Github repo:
-				</p>
+			<div className="flex items-center justify-center mt-14 ">
 				<a
 					href="https://github.com/realstoman/nextjs-firebase-auth"
 					target="__blank"
-					className="text-gray-200 hover:text-white cursor-pointer"
+					className="text-gray-700 hover:text-gray-900 cursor-pointer underline text-xl"
 				>
-					github.com/realstoman/nextjs-firebase-auth
+					Github repo
 				</a>
 			</div>
 		</>
